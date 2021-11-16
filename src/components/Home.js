7c45695d28b4dde556a56e2cac5d4c22f04e8652
Yue Hue <yuehu19931013@gmail.com>
2021-11-16 10:23:17 -0600

    add products files

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,26 @@
 import Product from "./Product";
+import "./Product.css";
+
 function Home() {
   return (
     <div id="content">
       <div id="middle_content">
-        <img
-          src="heroimage.jpg"
-          id="background_img"
-          className="center"
-          alt="background"
-        />
+        <div id="screen_off">
+          <img
+            src="heroimage.jpg"
+            id="background_img"
+            className="center"
+            alt="background"
+          />
+        </div>
         <div id="products">
-          <Product />
+          <Product
+            title="xixi"
+            image="iphone13ProMax.jpg"
+            price="50$"
+            rate="4.5"
+          />
+          {/* <Product title = "xixi" image = "haha" price = "50$" rate ="4.5" /> */}
         </div>
       </div>
     </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,11 @@
-function Product() {
+function Product({title, image, price, rate}) {
   return (
-    <div>
-      <div>title</div>
-      <div>image</div>
-      <div>price</div>
-      <div>rate</div>
+    <div id="product_content">
+      <div>{title}</div>
+      <img src={image} alt={title+ image} />
+      <div>{image}</div>
+      <div>{price}</div>
+      <div>{rate}</div>
       <div>add </div>
     </div>
   );
