40aa97dac1d561c0c1fb1ab24fa2027fd9b290ad
Yue Hue <yuehu19931013@gmail.com>
2021-11-16 21:15:38 -0600

    modify the content layout in products

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import Product from "./Product";
 import "./Product.css";
-
+import iphone13PM from "../assets/productsImg/iphone13ProMax.jpg";
 function Home() {
   return (
     <div id="content">
@@ -15,9 +15,9 @@ function Home() {
         </div>
         <div id="products">
           <Product
-            title="xixi"
-            image="iphone13ProMax.jpg"
-            price="50$"
+            title="Apple iPhone 13 Pro Max (1TB, Sierra Blue) [Locked] + Carrier Subscription"
+            image={iphone13PM}
+            price="1299"
             rate="4.5"
           />
           {/* <Product title = "xixi" image = "haha" price = "50$" rate ="4.5" /> */}
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,22 @@
-function Product({title, image, price, rate}) {
+function Product({ title, image, price, rate }) {
   return (
     <div id="product_content">
-      <div>{title}</div>
-      <img src={image} alt={title+ image} />
-      <div>{image}</div>
-      <div>{price}</div>
-      <div>{rate}</div>
-      <div>add </div>
+      <div id="product_top">
+        <div id="product_title">{title}</div>
+        <div id="off"></div>
+        <div id="product_price">
+          <span id="dollar_sign">$</span>
+          <span id="product_price_number">{price}</span>
+        </div>
+        <div id="product_rate">{rate}</div>
+      </div>
+
+      <div id="product_image">
+        <img src={image} alt="" />
+      </div>
+
+      <div id="product_bottom"><button id="product_add_button">Add to Cart</button>  </div>
+      <div></div>
     </div>
   );
 }
